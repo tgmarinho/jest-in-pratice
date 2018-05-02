54de1e9736cab0b38f19de2087afbb55144e83fe
Thiago Marinho <tgmarinho@gmail.com>
2018-05-02 15:15:30 -0400

    TDD every

---
+++ src/every/every.js
@@ -0,0 +1,12 @@
+const every = (arr, func) => {
+   for(let i =0; i < arr.length; i++) {
+       if(!func(arr[i])) {
+           return false
+       }
+   }
+   
+   return true;
+
+}
+
+export default every;
