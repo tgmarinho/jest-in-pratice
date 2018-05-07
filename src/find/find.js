9f13308f2edc6ed3d046c23773b72e5fb269ad45
Thiago Marinho <tgmarinho@gmail.com>
2018-05-07 15:24:31 -0400

    find impl complete

--- src/find/find.js
+++ src/find/find.js
@@ -1,6 +1,6 @@
 const find = (arr, func) => {
-    for(let i = 0; i < arr.length; i++){
-        if(func(arr[i])){
+    for(let i = 0; i <= arr.length; i++){
+        if(func(arr[i], i, arr)){
             return arr[i]
         }
     }
--- src/find/find.test.js
+++ src/find/find.test.js
@@ -9,6 +9,16 @@ it('find([1,2,3], item => (item) === 1) should return 1', () => {
     expect(find([1,2,3], (item) => item === 1)).to.be.equal(1);
 });
 
-it('find([1,2,3], item => (item) === 3) should return 3', () => {
+it('find([1,2,3], item => item === 3) should return 3', () => {
     expect(find([1,2,3], (item) => item === 3)).to.be.equal(3);
 });
+
+it('find([1,2,3], (item, index) => index === 1) should return 2', () => {
+    expect(find([1,2,3], (item, index) => index === 1)).to.be.equal(2);
+});
+
+it('find([1,2,3], (item, index, array) => array[index] === 2) should return 2', () => {
+    expect(find([1,2,3], (item, index, array) => array[index] === 2)).to.be.equal(2);
+});
+
+
