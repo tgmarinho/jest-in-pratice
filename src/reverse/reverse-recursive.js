60d51c67c41a9f340cf48da585b67ecb87815ee3
Thiago Marinho <tgmarinho@gmail.com>
2018-05-07 11:28:54 -0400

    impl reverse recursive functional

--- src/reverse/reverse-recursive.js
+++ src/reverse/reverse-recursive.js
@@ -1,9 +1,8 @@
 const reverse = (arr) => {
- if(arr.length === 0) {
-     return []
- }
-
- return [arr[arr.length - 1]].concat(reverse(arr.slice(0, -1)))
+  return arr.length === 0 ? [] :
+  [
+    arr.slice(-1)[0], ...reverse(arr.slice(0, -1))
+  ]
 }
 
 export default reverse;
