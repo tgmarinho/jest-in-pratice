9ea2eb583e4e948a322fa707b06bcd99043171c4
Thiago Marinho <tgmarinho@gmail.com>
2018-05-07 14:52:44 -0400

    impl reduceRigth

---
+++ src/reduce/reduce-right.js
@@ -0,0 +1,7 @@
+
+import mainReduce from './main-reduce'
+import reverse from '../reverse/reverse'
+
+const reduceRight = (arr, ...initialValue) => { mainReduce(reverse(arr), ...params) }
+
+export default reduceRight
--- src/reduce/reduce.test.js
+++ src/reduce/reduce.test.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import reduce from './reduce'
+import reduceRight from './reduce-right'
 
 it('reduce should be a function', () => {
     expect(reduce).to.be.a('function')
@@ -26,5 +27,11 @@ it('reduce([2,3,4], (acc, item) => acc + item) should return 9', () => {
 it('reduce([1,2], (acc, item) => acc + item, 0) should return 1', () => {
     const before = reduce([1,2], (acc, item, index) => acc + index, 0) 
     const after = 1;
+    expect(before).to.be.equal(after);
+});
+
+it('reduceRight(["do", "nan", "fer"], (acc, item) => acc + item, "") should return "fernando"', () => {
+    const before = reduceRight(["do", "nan", "fer"], (acc, item) => acc + item, "")
+    const after = "fernando";
     expect(before).to.be.equal(after);
 });
